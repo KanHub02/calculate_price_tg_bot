543fac8f16e8a23d3460501dc8b6974f0f8de4ca
KanHub02 <usmvnk200207@gmail.com>
2024-05-25 21:42:14 +0600

    Modifird: settings.py

--- src/web/config/settings.py
+++ src/web/config/settings.py
@@ -35,7 +35,8 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     "jazzmin",
     "django.contrib.admin",
-    "django.contrib.auth",
+    "common.apps.CustomAuthConfig",
+    # "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
