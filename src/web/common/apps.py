54e6c71e27e243919840a903af8cca8f6b588969
KanHub02 <usmvnk200207@gmail.com>
2024-05-25 21:31:42 +0600

    Modified: verbose_name for apps

--- src/web/common/apps.py
+++ src/web/common/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class CommonConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "common"
+
+
+
+class CustomAuthConfig(AppConfig):
+    name = 'django.contrib.auth'
+    verbose_name = 'Пользователи'
